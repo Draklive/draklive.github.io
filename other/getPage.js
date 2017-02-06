9471fe8ccb0de1189123e12e7a2e39e955719d99
Draklive <eriwes1ros@rosendal.local>
2017-02-06 12:20:42 +0100

    commit

---
+++ other/getPage.js
@@ -0,0 +1,12 @@
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     document.getElementById("content").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "pages/skolan.txt", true);
+  xhttp.send();
+}
+
+loadDoc();
